648d74acf9b7a9ddb0b70f0eb4336ccba1c20ad2
Vladimir <metenevvv@gmail.com>
2020-12-04 10:09:19 +0300

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+class CFG:
+    max_grad_norm=1000
+    gradient_accumulation_steps=1
+    hidden_size=512
+    dropout=0.5
+    lr=1e-2
+    weight_decay=1e-6
+    batch_size=32
+    epochs=30
+    #total_cate_size=5
+    #emb_size=4
+    num_features=num_features
+    cat_features=cat_features
+    target_cols=target_cols
+    
